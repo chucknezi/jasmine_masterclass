2814d4f71362ef7aaf113bc126a8b6ba4de29c52
ChuckNezi <jnezia1@gmail.com>
2019-08-05 05:49:34 -0400

    learned how to manage change detection

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -32,6 +32,10 @@ beforeEach( async(() => {
 
     it('should display the course list', () => {
         component.courses = setupCourses();
+
+        fixture.detectChanges();
+        // console.log(el.nativeElement.outerHTML)
+
         const cards = el.queryAll(By.css(".course-card"))
         expect(cards).toBeTruthy("Could not find cards")
         expect(cards.length).toBe(12, "unexpected")
