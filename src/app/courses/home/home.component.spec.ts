99acd44ed79180fd1f2b1618c4c767a70e5f49a4
ChuckNezi <jnezia1@gmail.com>
2019-08-07 09:24:57 -0400

    learned to work with container component test suite

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -26,6 +26,9 @@ describe('HomeComponent', () => {
   const beginnerCourses = setupCourses()
     .filter(course => course.category === 'BEGINNER')
 
+    const advancedCourses = setupCourses()
+    .filter(course => course.category === 'ADVANCED')
+
   beforeEach(async(() => {
 
     const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
@@ -63,13 +66,19 @@ describe('HomeComponent', () => {
 
 
   it("should display only advanced courses", () => {
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found')
 
-      
   });
 
 
   it("should display both tabs", () => {
-
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(2  , 'Expected two tabs to be found')
       
   });
 
